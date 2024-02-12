579f508c8347f8bf344339da57d3f6d4001eb04d
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2024-02-12 19:22:20 +0300

    Happy new year in abit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("January 01, 2024 00:00:00").getTime();
+    const countDate = new Date("January 01, 2025 00:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
